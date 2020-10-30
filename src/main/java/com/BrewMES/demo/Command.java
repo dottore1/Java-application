a7af38d6a0dd75169f823268b3ff574ab4e74b6f
Teyson <teis@aalbaek-nielsen.dk>
2020-10-30 10:20:01 +0100

    Change to enum classes

--- src/main/java/com/BrewMES/demo/Command.java
+++ src/main/java/com/BrewMES/demo/Command.java
@@ -1,10 +1,9 @@
 package com.BrewMES.demo;
 
-public class Command {
-	private final int RESET = 1;
-	private final int START = 2;
-	private final int STOP = 3;
-	private final int ABORT = 4;
-	private final int CLEAR = 5;
-
+public enum Command {
+	RESET,
+	START,
+	STOP,
+	ABORT,
+	CLEAR
 }
