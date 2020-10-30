3c5f091935c5c41ccc0b0485e7531b65c2d5a606
Teyson <teis@aalbaek-nielsen.dk>
2020-10-30 08:49:23 +0100

    Initial commit of Spring files and small additions to .gitignore

---
+++ src/main/java/com/BrewMES/demo/BrewMesApplication.java
@@ -0,0 +1,13 @@
+package com.BrewMES.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewMesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewMesApplication.class, args);
+	}
+
+}
