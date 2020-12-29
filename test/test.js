b81f67ebec6e3e7ca1b4bac9c864150a0dcc54c6
dottore1 <54813290+dottore1@users.noreply.github.com>
2020-12-29 01:51:08 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -7,8 +7,19 @@ newman.run({
     bail: true
 }).on('done', (err, summary) => {
     if(err || summary.run.failures.length){
-        console.log(err);
-        process.exit(1);
+      const failureCount = summary.run.failures.length;
+      summary.run.failures.forEach((element, index) => {
+        const header = `${'='.repeat(32)}[ Error-${index + 1} ]${'='.repeat(32)}`;
+         console.error(header);
+        console.error('Tag      :', element.parent.name);
+        console.error('Source   :', element.source.name);
+        console.error('Type     :', element.error.name);
+        console.error('Test     :', element.error.test);
+         console.error('Error    :', element.error.message);
+      if (index === failureCount - 1) console.error('~'.repeat(header.length));
+    });
+    console.error(treeify.asTree(summary.run.stats, true));
+    process.exit(1);
     } else{
         process.exit(0);
     }
