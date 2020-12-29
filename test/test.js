adf86a9e88246d530e064408d85dd4fc458be6f4
dottore1 <54813290+dottore1@users.noreply.github.com>
2020-12-29 01:17:08 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,7 +2,8 @@ const newman = require('newman');
 
 newman.run({
     collection: require('./test_collection.json'),
-    reporters: 'cli'
+    reporters: 'cli',
+    bail: true
 }, function (err) {
     if (err) { throw err; }
     console.log('collection run complete!');
