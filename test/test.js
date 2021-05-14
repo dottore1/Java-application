2109d6a3fb1f680ead82358cf06cdad950c5cee7
dottore1 <54813290+dottore1@users.noreply.github.com>
2021-05-14 22:57:29 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -5,22 +5,16 @@ newman.run({
     collection: require('./test_collection.json'),
     reporters: 'cli',
     bail: true
-}).on('done', (err, summary) => {
-    if(err || summary.run.failures.length){
-      const failureCount = summary.run.failures.length;
-      summary.run.failures.forEach((element, index) => {
-        const header = `${'='.repeat(32)}[ Error-${index + 1} ]${'='.repeat(32)}`;
-         console.error(header);
-        console.error('Tag      :', element.parent.name);
-        console.error('Source   :', element.source.name);
-        console.error('Type     :', element.error.name);
-        console.error('Test     :', element.error.test);
-         console.error('Error    :', element.error.message);
-      if (index === failureCount - 1) console.error('~'.repeat(header.length));
-    });
-    console.error(treeify.asTree(summary.run.stats, true));
-    process.exit(1);
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+    if (err) { 
+        errorFlag = true;
+        throw err; 
+    }
+    if(!errorFlag){
+        console.log('collection run complete!');
     } else{
-        process.exit(0);
+        process.exit(-1);
     }
 });
